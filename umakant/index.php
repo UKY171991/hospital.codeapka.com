2275e4c7b700c0f74f41d2db70b22dff02c41f00
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-08-19 12:17:47 +0530

    d

--- umakant/index.php
+++ umakant/index.php
@@ -2,8 +2,8 @@
 <?php
 // index.php
 // Main entry for AdminLTE 3 PHP template
-include 'header.php';
-include 'navbar.php';
-include 'sidebar.php';
+include 'inc/header.php';
+include 'inc/navbar.php';
+include 'inc/sidebar.php';
 include 'content.php';
-include 'footer.php';
+include 'inc/footer.php';
