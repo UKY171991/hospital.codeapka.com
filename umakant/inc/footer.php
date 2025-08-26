12dbdf6c31ec21b577ee770992854a1a73fa17e3
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-08-26 14:37:45 +0530

    df

--- umakant/inc/footer.php
+++ umakant/inc/footer.php
@@ -8,4 +8,6 @@
     <script src="assets/js/app.js"></script>
     </div>
     </body>
+        <!-- DataTables JS -->
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     </html>
--- umakant/inc/header.php
+++ umakant/inc/header.php
@@ -4,6 +4,8 @@
 <!doctype html>
 <html lang="en">
 <head>
+  <!-- DataTables CSS -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Hospital Admin</title>
