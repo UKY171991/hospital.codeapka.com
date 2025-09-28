64884375a4476af8cfe6c0263d7a5267a9656a0c
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-09-28 15:06:35 +0530

    Update tmp_owner_test.php

--- umakant/tmp_owner_test.php
+++ umakant/tmp_owner_test.php
@@ -2,7 +2,8 @@
 $ctx = stream_context_create([
     'http' => [
         'ignore_errors' => true,
-        'timeout' => 10,
+        'timeout' => 15,
+        'header' => "Accept: application/json\r\nAccept-Encoding: identity\r\n"
     ],
 ]);
 $url = 'https://hospital.codeapka.com/umakant/ajax/owner_api.php?action=list';
@@ -10,3 +11,7 @@ $response = file_get_contents($url, false, $ctx);
 var_dump($http_response_header ?? []);
 echo "\n=== RESPONSE ===\n";
 echo $response;
+
+$decoded = json_decode($response, true);
+echo "\n=== DECODED ===\n";
+var_dump($decoded);
