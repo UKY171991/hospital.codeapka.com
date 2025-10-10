0e116edbe90b9a5a7ae94903199036fc64fdbfb0
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-10-10 14:31:08 +0530

    df

--- umakant/inc/category_view_modal.php
+++ umakant/inc/category_view_modal.php
@@ -1,14 +1,14 @@
 <div class="modal fade" id="categoryViewModal" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Category Details</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div class="modal-header bg-info text-white">
+        <h5 class="modal-title mb-0"><i class="fas fa-eye mr-2"></i>Category Details</h5>
+        <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        <div class="category-view-content">
+      <div class="modal-body p-0">
+        <div class="list-group list-group-flush" id="categoryViewContent">
           <!-- Category details injected here -->
         </div>
       </div>
