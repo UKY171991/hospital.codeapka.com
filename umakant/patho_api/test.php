52453bc9e230d4acf86c5671f6e609c0fb42e132
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-10-12 11:10:30 +0530

    sdf

--- umakant/patho_api/test.php
+++ umakant/patho_api/test.php
@@ -2,8 +2,13 @@
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-API-Key, X-API-Secret');
+
 // Centralized error handler
-function handle_error($errno, $errstr, $errfile, $errline) {
+function handle_error($errno, $errstr, $errfile, $errline) { 
     $error_data = [
         'success' => false,
         'message' => 'An unexpected error occurred.',
