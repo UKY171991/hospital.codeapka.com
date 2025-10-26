cc6e325c68e0452b39876e94fe95a371eb32d1b6
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-10-27 03:08:07 +0530

    Update entry-list.js

--- umakant/assets/js/entry-list.js
+++ umakant/assets/js/entry-list.js
@@ -1030,7 +1030,7 @@ class EntryManager {
                 data: { action: 'get', id: entryId },
                 dataType: 'json'
             });
-            console.log('Displaying entry response:', response.data);
+            console.log('Displaying entry response:', response);
             if (response.success && response.data) {
                 this.displayEntryDetails(response.data);
                 $('#viewEntryModal').modal('show');
