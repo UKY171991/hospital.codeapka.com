58e8fdb3d8afa1b28c3fec4075ef8ef77a31a560
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-11-17 13:07:56 +0530

    Update inventory_api.php

--- umakant/ajax/inventory_api.php
+++ umakant/ajax/inventory_api.php
@@ -4,6 +4,9 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
