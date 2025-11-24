c92e4b493f52bec3ee48f8c4d889f50f662693e2
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-11-24 11:48:56 +0530

    Update opd_reports.php

--- umakant/opd_reports.php
+++ umakant/opd_reports.php
@@ -287,16 +287,6 @@ require_once 'inc/sidebar.php';
     </div>
 </div>
 
-<!-- Include DataTables CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
-
-<!-- Include DataTables JS -->
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
-
-<!-- Page specific JavaScript -->
-<script src="assets/js/opd_reports.js"></script>
-
 <style>
 .small-box {
     position: relative;
